da592cc127c7a792503f9f14e252e3873ae468c3
Pape Barka <papebarka@gmail.com>
2024-06-06 17:36:49 +0000

    Refactor the info function with pattern matching

--- account.erl
+++ account.erl
@@ -1,15 +1,15 @@
 %% @doc Functions related to user accounts.
 
 -module(account).
--export([info/1, info/2]).
+-export([info/1]).
 
 
 %% @doc Returns the customer account information
 %% when his/her account number has been provided
 
--spec(info(number()) -> tuple()).
-info(Account_number) -> Account_number.
+-spec(info(tuple()) -> fun()).
+info({Account_Type, Account_Number}) -> info(Account_Type, Account_Number).
 
--spec(info(atom(), number()) -> number()).
+-spec(info(atom(), number()) -> tuple()).
 info(individual, Account_number) -> Account_number;
 info(corporate, Account_number) -> Account_number.
