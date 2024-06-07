22f4d5342f48e78c77ece7855d5e7558399db315
Pape Barka <papebarka@gmail.com>
2024-06-07 12:50:17 +0000

    Simulates a dummy transactions historic function with recursion

--- account.erl
+++ account.erl
@@ -1,7 +1,7 @@
 %% @doc Functions related to user accounts.
 
 -module(account).
--export([info/1]).
+-export([info/1, transactions_historic/1]).
 
 
 %% @doc Returns the customer account information
@@ -13,3 +13,12 @@ info({Account_Type, Account_Number}) -> info(Account_Type, Account_Number).
 -spec(info(atom(), number()) -> tuple()).
 info(individual, Account_number) -> Account_number;
 info(corporate, Account_number) -> Account_number.
+
+%% @doc Returns customer's transactions historic
+
+transactions_historic({Account_number, Limit}) when Limit > 0 ->
+    io:format("~w~n", [Limit]),
+    transactions_historic({Account_number, Limit - 1});
+
+transactions_historic({_Account_number, _Limit}) ->
+    io:format("You have reached the historic limit.").
---
+++ operations.erl
@@ -0,0 +1,9 @@
+%% @doc Functions related to customers operations
+
+-module(operations).
+-export([transfer/1]).
+
+-spec(transfer({number(), number()}) -> tuple()).
+transfer({{Amount, Account}}) ->
+    
+    "Hello".
